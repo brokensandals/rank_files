0699f6589c494028b92be4b7518d10d66240bf15
Jacob Williams <jacobaw@gmail.com>
2025-04-13 08:30:17 -0700

    remove deduplication code - unnecessary complexity

--- src/rank_files/document.py
+++ src/rank_files/document.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 from hashlib import sha256
-from collections import defaultdict
 
 
 class Document(ABC):
@@ -51,16 +50,3 @@ class StrDocument(Document):
     
     def __str__(self) -> str:
         return self.text
-
-
-class DocumentSet:
-    def __init__(self, paths: set[Path]):
-        self.documents = [FileDocument(path) for path in sorted(paths)]
-
-        by_hash = defaultdict(list)
-        for doc in self.documents:
-            by_hash[doc.hash()].append(doc)
-        
-        duplicates = [[doc.path for doc in dups] for dups in by_hash.values() if len(dups) > 1]
-        if len(duplicates) > 0:
-            raise ValueError(f"Found the following sets of duplicate files; duplicates are not supported: {duplicates}")
--- tests/test_document.py
+++ tests/test_document.py
@@ -1,24 +1,17 @@
-import pytest
-from rank_files.document import DocumentSet
+from rank_files.document import FileDocument
 
 
-def test_duplicates(tmp_path):
-    p1 = tmp_path / "file1.txt"
-    p1.write_text("Hello", "utf-8")
-    p2 = tmp_path / "file2.txt"
-    p2.write_text("Hello", "utf-8")
-    with pytest.raises(ValueError):
-        DocumentSet({p1, p2})
-
-
-def test_no_duplicates(tmp_path):
+def test_file_document(tmp_path):
     p1 = tmp_path / "file1.txt"
     p1.write_text("Hello", "utf-8")
     p2 = tmp_path / "file2.txt"
     p2.write_text("Goodbye", "utf-8")
-    docset = DocumentSet({p1, p2})
-    assert len(docset.documents) == 2
-    assert docset.documents[0].path == p1
-    assert docset.documents[0].hash() == "185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969"
-    assert docset.documents[1].path == p2
-    assert docset.documents[1].hash() == "c015ad6ddaf8bb50689d2d7cbf1539dff6dd84473582a08ed1d15d841f4254f4"
+    p3 = tmp_path / "file3.txt"
+    p3.write_text("Hello", "utf-8")
+    doc1, doc2, doc3 = [FileDocument(p) for p in [p1, p2, p3]]
+    assert doc1.path == p1
+    assert doc1.hash() == "185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969"
+    assert doc2.path == p2
+    assert doc2.hash() == "c015ad6ddaf8bb50689d2d7cbf1539dff6dd84473582a08ed1d15d841f4254f4"
+    assert doc3.path == p3
+    assert doc3.hash() == "185f8db32271fe25f561a6fc938b2e264306ec304eda518007d1764826381969"
